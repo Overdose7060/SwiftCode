0a755ee66ca9f1ac98d8c69d838064303717a572
admin <admin@admins-MacBook-Pro.local>
2024-06-22 20:05:10 -0700

    FirstCommit

---
+++ Struct/Struct/StructApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructApp.swift
+//  Struct
+//
+//  Created by admin on 21/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct StructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
